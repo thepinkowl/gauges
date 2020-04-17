c7b771c757cc813c2f750824f1d2027b2e3f2724
rogerxaic <roger.miret@gmail.com>
2020-04-17 17:45:18 +0200

    add scss config

--- src/App.svelte
+++ src/App.svelte
@@ -7,19 +7,19 @@
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
-<style>
+<style lang="scss">
 	main {
 		text-align: center;
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+		h1 {
+			color: #ff3e00;
+			text-transform: uppercase;
+			font-size: 4em;
+			font-weight: 100;
+		}
 	}
 
 	@media (min-width: 640px) {
