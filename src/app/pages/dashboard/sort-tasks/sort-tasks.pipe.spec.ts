d6ab234b9f4444d78e32afdc356b8b2bcaf90ee8
Valentin Viennot <misterw97@users.noreply.github.com>
2020-06-13 20:50:19 +0200

    remove unused tests

--- src/app/pages/dashboard/sort-tasks/sort-tasks.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SortTasksPipe } from './sort-tasks.pipe';
-
-describe('SortTasksPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SortTasksPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
