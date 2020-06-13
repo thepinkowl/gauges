164f3f3d37bddba0e99017f09ad9233342d85eb0
Valentin Viennot <misterw97@users.noreply.github.com>
2020-06-13 20:20:03 +0000

    fix tasks sorting

--- src/app/pages/dashboard/sort-tasks/sort-tasks.pipe.ts
+++ src/app/pages/dashboard/sort-tasks/sort-tasks.pipe.ts
@@ -7,10 +7,10 @@ import Task from 'src/app/models/Task';
 export class SortTasksPipe implements PipeTransform {
 
   transform(tasks: Task[]): Task[] {
-    if (!tasks) return tasks;
+    if (!tasks) { return tasks; }
     tasks.sort((a: Task, b: Task) => {
-      const diff = b.progress - a.progress;
-      if (diff === 0) return b.lastDone.getTime() - a.lastDone.getTime();
+      const diff = a.progress - b.progress;
+      if (diff === 0) { return a.lastDone.getTime() - b.lastDone.getTime(); }
       return diff;
     });
     return tasks;
