5de81a496627575f58b092ec26c9716fb8bf4ab1
rogerxaic <roger.miret@gmail.com>
2021-03-21 13:13:16 +0100

    Better hide header behaviour

--- src/app/directives/hide-header.directive.ts
+++ src/app/directives/hide-header.directive.ts
@@ -1,5 +1,5 @@
 // Source: https://stackoverflow.com/a/56996295
-import { Directive, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
 import { DomController } from '@ionic/angular';
 
 @Directive({
@@ -8,29 +8,57 @@ import { DomController } from '@ionic/angular';
 export class HideHeaderDirective implements OnInit {
 
   search: any;
-
-  private lastY = 0;
+  used = false;
 
   constructor(
     private renderer: Renderer2,
-    private domCtrl: DomController
+    private domCtrl: DomController,
+    public el: ElementRef
   ) { }
 
   ngOnInit(): void {
-    this.search = document.getElementById("dashboard-search-bar")    
+    this.search = document.getElementById("dashboard-search-bar")
+    setTimeout(() => {
+      if (!this.used) this.scrollPage(1);
+    }, 700)
+  }
+
+  showSearchBar() {
+    this.domCtrl.write(() => {
+      this.renderer.setStyle(this.search, 'height', `60px`);
+    });
+  }
+  hideSearchBar() {
+    this.domCtrl.write(() => {
+      this.renderer.setStyle(this.search, 'height', `0`);
+    });
+  }
+
+  scrollPage(px: number) {
+    let page = this.el.nativeElement;
+    let scroller = [...page.shadowRoot.childNodes].find(n => n.className.includes("inner-scroll"));
+    scroller.scrollTop = px;
   }
 
   @HostListener('ionScroll', ['$event']) onContentScroll($event: any) {
-    if ($event.detail.scrollTop > this.lastY) {
-      this.domCtrl.write(() => {
-        this.renderer.setStyle(this.search, 'height', `0`);
-      });
-    } else {
-      this.domCtrl.write(() => {
-        this.renderer.setStyle(this.search, 'height', `60px`);
-      });
-    }
+    this.used = true;
 
-    this.lastY = $event.detail.scrollTop;
+    if ($event.detail.deltaY < 0) {
+      if ($event.detail.scrollTop === 0) {
+        if ($event.detail.deltaY !== -1) {
+          this.scrollPage(1);
+        } else {
+          this.showSearchBar();
+        }
+      }
+    } else if ($event.detail.deltaY > 1) {
+      this.hideSearchBar();
+    } else {
+      if ($event.detail.scrollTop === 0) {
+        this.showSearchBar();
+      } else {
+        this.hideSearchBar();
+      }
+    }
   }
 }
