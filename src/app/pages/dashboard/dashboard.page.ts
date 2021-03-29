92711fab58bd4acda8b68d482a057029f9ff5c9a
rogerxaic <roger.miret@gmail.com>
2021-03-29 09:09:36 +0200

    improve search

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,76 +1,104 @@
-import { Component, OnDestroy } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
-import Task from 'src/app/models/Task';
-import { CategoriesService, Category, OtherCategory } from 'src/app/services/categories.service';
-import { TasksService } from '../../services/tasks.service';
+import { Component, ElementRef, OnDestroy } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
+import Task from "src/app/models/Task";
+import {
+  CategoriesService,
+  Category,
+  OtherCategory,
+} from "src/app/services/categories.service";
+import { TasksService } from "../../services/tasks.service";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: 'dashboard.page.html',
-  styleUrls: ['dashboard.page.scss'],
+  selector: "app-dashboard",
+  templateUrl: "dashboard.page.html",
+  styleUrls: ["dashboard.page.scss"],
 })
 export class DashBoardPage implements OnDestroy {
   constructor(
     private tasksService: TasksService,
     public categoriesService: CategoriesService,
-  ) { }
+    public el: ElementRef
+  ) {}
 
   public $categories: Observable<Category[]>;
   public categories: Category[] = [];
+  public categoryCIDs: string[] = [];
   public initialCategories: Category[];
   public $tasks: Observable<Task[]>;
   public tasks: Task[] = [];
   private initialTasks: Task[] = [];
   subs: Subscription[] = [];
 
-  showTasksRatherThanCategories = true;
+  showTasksRatherThanCategories = false;
 
   ngOnDestroy() {
-    this.subs.forEach(s => s.unsubscribe());
+    this.subs.forEach((s) => s.unsubscribe());
   }
 
   ionViewWillEnter() {
     this.$tasks = this.tasksService.getTasks();
     this.$categories = this.categoriesService.getCategories();
-    const taskSubscription = this.$tasks.subscribe(tasks => {
-      this.initialTasks = tasks
-      this.tasks = tasks
-    })
-    const categoriesSubscription = this.$categories.subscribe(categories => {
-      this.initialCategories = categories
-      this.categories = categories
-    })
-    this.subs.push(taskSubscription, categoriesSubscription)
+    const taskSubscription = this.$tasks.subscribe((tasks) => {
+      this.initialTasks = tasks;
+      this.tasks = tasks;
+    });
+    const categoriesSubscription = this.$categories.subscribe((categories) => {
+      this.initialCategories = categories;
+      this.categories = categories;
+    });
+    this.subs.push(taskSubscription, categoriesSubscription);
+  }
+
+  ionViewDidEnter() {
+    this.scrollPage(60);
+  }
+
+  scrollPage(px: number) {
+    let page = this.el.nativeElement.firstChild;
+    let scroller = [...page.shadowRoot.childNodes].find((n) =>
+      n.className.includes("inner-scroll")
+    );
+    scroller.scrollTop = px;
   }
 
   get parentCategories() {
-    let out = [...this.categories.filter(c => {
-      const children = this.getChildCategories(c);
-      const shouldShow = children.some(child => this.tasks.some(t => t.category === child.cid))
-      return !c.parentcid && shouldShow
-    })];
-    if (this.tasks.some(t => !t.hasCategory())) {
-      out.push(OtherCategory)
+    let out = [
+      ...this.categories.filter((c) => {
+        const children = this.getChildCategories(c);
+        const shouldShow = children.some(
+          (child) =>
+            this.tasks.some((t) => t.category === child.cid) ||
+            this.categoryCIDs.some((ca) => child.cid === ca)
+        );
+
+        return !c.parentcid && shouldShow;
+      }),
+    ];
+    if (this.tasks.some((t) => !t.hasCategory())) {
+      out.push(OtherCategory);
     }
-    return out
+    return out;
   }
 
   get otherTasks() {
-    return this.tasks.filter(t => !t.hasCategory())
+    return this.tasks.filter((t) => !t.hasCategory());
   }
 
   getChildCategories(cat: Category) {
-      return this.categories.filter(c => {
-        const shouldShow = this.tasks.some(t => t.category === c.cid)
-        return c.parentcid === cat.cid && shouldShow
-      })
-    }
+    return this.categories.filter((c) => {
+      const shouldShow =
+        this.tasks.some((t) => t.category === c.cid) ||
+        (this.categoryCIDs.some((ca) => c.cid === ca) &&
+          this.initialTasks.some((t) => t.category === c.cid));
+      return c.parentcid === cat.cid && shouldShow;
+    });
+  }
 
   getCategoryProgress(category: Category) {
     const tasks = this.tasksService.getTasksInCategory(category);
-    
+
     const progressType = "worst";
-    const progresses = tasks.map(t => t.progress);
+    const progresses = tasks.map((t) => t.progress);
 
     if (progressType === "worst") {
       return Math.min(...progresses);
@@ -82,8 +110,13 @@ export class DashBoardPage implements OnDestroy {
   }
 
   filterContent(event) {
-    this.tasks = this.initialTasks.filter(t => t.name.toLowerCase().includes(event.target.value.toLowerCase()))
-    this.categories = this.initialCategories.filter(t => t.title.toLowerCase().includes(event.target.value.toLowerCase()))
+    this.tasks = this.initialTasks.filter((t) =>
+      t.name.toLowerCase().includes(event.target.value.toLowerCase())
+    );
+    this.categoryCIDs = this.initialCategories
+      .filter((c) =>
+        c.title.toLowerCase().includes(event.target.value.toLowerCase())
+      )
+      .map((c) => c.cid);
   }
-
 }
